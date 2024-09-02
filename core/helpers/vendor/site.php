80249116c2f76a107752d3f6f5173af467bc3b2d
nurmuhass <nurmuhass@gmail.com>
2024-09-02 05:45:39 +0100

    first commit

---
+++ core/helpers/vendor/site.php
@@ -0,0 +1,9 @@
+<?php
+
+    //Configuration Settings
+    $sitename= "ADU BUSINESS";
+    $parentdirectory = "";
+    $secret_key="e3371b55bdfd2c7f9f804075717b601c";
+    $public_key="e3371b55bdfd2c7f9f804075717b601c";
+   
+?>
